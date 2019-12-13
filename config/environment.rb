8783be37a7dfafd9ee8d32b1f686c6800411da70
Olivia Kesler <oakesler@protonmail.ch>
2019-12-13 05:19:43 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -8,9 +8,9 @@ require 'date'
 require 'bundler/setup'
 Bundler.require
 
+require_relative "../artist.rb"
+
 ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
-
-require_relative "../artist.rb"
