1aa70a6ef26eda0de332034967837f8c8cee2632
Olivia Kesler <oakesler@protonmail.ch>
2019-12-13 05:15:44 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -12,6 +12,5 @@ ActiveRecord::Base.establish_connection(
   :adapter => "sqlite3",
   :database => "db/artists.sqlite"
 )
-# put the code to connect to the database here
 
 require_relative "../artist.rb"
