f893eec5a204953072a1c53dce91daba79512bd5
Olivia Kesler <oakesler@protonmail.ch>
2019-12-13 05:16:16 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -14,16 +14,3 @@ class CreateArtists < ActiveRecord::Migration[5.2]
     end
   end
 end
-
-
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
- 
-ActiveRecord::Base.connection.execute(sql)
